f3923efea44520838d2bfd5c7f44ab1966423b99
laurent sanchez <ext@galerieslafayette.com>
2017-04-07 14:50:34 +0200

    add contact header page

--- src/app/containers/contact/contact.container.ts
+++ src/app/containers/contact/contact.container.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { select } from '@angular-redux/store';
+import { Observable } from 'rxjs/Rx';
+import { IHeader } from '../../store/main/main';
 
 @Component({
   selector: 'bz-contact',
   templateUrl: './contact.html'
 })
 export class ContactComponent {
+  @select(state => state.mainReducer.header) header: Observable<IHeader>;
   constructor() { }
 }
