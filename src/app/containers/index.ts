20e0ed5fac4171e1613341a569b36461ae5688e6
Laurent Sanchez <laurentsanchez@mbpdelaurent2.home>
2017-04-10 01:07:02 +0200

    fix containers error

--- src/app/containers/index.ts
+++ src/app/containers/index.ts
@@ -1,9 +1,7 @@
 import { ContactComponent } from './contact/contact.container';
 import { MainComponent } from './main/main.container';
-import { PageComponent } from './page/page.container';
 
 export const containers = [
   ContactComponent,
-  MainComponent,
-  PageComponent
+  MainComponent
 ];
